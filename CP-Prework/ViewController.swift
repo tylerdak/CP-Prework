2c26eac78b4b5f47ab216e00869bd632f3a35b9a
tylerdak <dakint@icloud.com>
2022-01-11 13:20:27 -0500

    Initial Commit

---
+++ CP-Prework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CP-Prework
+//
+//  Created by Tyler Dakin on 1/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CP-PreworkUITests/CP_PreworkUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CP_PreworkUITestsLaunchTests.swift
+//  CP-PreworkUITests
+//
+//  Created by Tyler Dakin on 1/11/22.
+//
+
+import XCTest
+
+class CP_PreworkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
